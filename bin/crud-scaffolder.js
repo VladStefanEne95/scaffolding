71c5d671815080d13a1a37dd49e75accf2ee4c0b
Vlad Ene <ankino_kara@yahoo.com>
2017-05-24 12:12:03 +0300

    create and init folder

--- bin/crud-scaffolder.js
+++ bin/crud-scaffolder.js
@@ -9,75 +9,102 @@ var angModelGenerator = require('../lib/angularModelGenerator');
 var angDetailGenerator = require('../lib/detail-generator');
 var angServiceGenerator = require('../lib/service-generator');
 var angRouteGenerator = require('../lib/route-generator');
+var angAppGenerator = require('../lib/app-generator');
 var angComponentGenerator = require('../lib/component-generator');
 var controllerGenerator = require('../lib/controller-generator');
 var viewsGenerator = require('../lib/view-generator');
 var capitalize = require('../lib/utils').capitalize;
-var inputul = require('../lib/scaffolding-folder/index');
+var inputul = require('../lib/server/index');
 
-var allowedTypes = ['string', 'number', 'date', 'boolean', 'array'];
+var allowedTypes_server = ['string', 'number', 'date', 'boolean', 'array'];
+var allowedTypes_client = ['string', 'number', 'date', 'boolean', 'array', 'any'];
 
-var ang_scaffold = require('../lib/scaffolding-folder/angularVar');
+var client_input = require('../lib/client/index');
+
+
+  var args = process.argv.slice(2);
+
+  if (!args.length) {
+    showUsage('Introduceti numele folderului');
+  }
+
+  var folderName = args.shift().toLowerCase();
+  if (!fs.existsSync(folderName))
+      fs.mkdir(folderName);
+
+  while (!fs.existsSync(folderName));
+    
+  process.chdir(folderName);
+  const exec = require('child_process').exec;
+  exec('npm init --yes');
+
 
   if (!fs.existsSync('ang-app'))
     fs.mkdir('ang-app');
-
-angModelGenerator.angGenerateModel(ang_scaffold);
-angDetailGenerator.angGenerateDetail(ang_scaffold);
-angServiceGenerator.angGenerateService(ang_scaffold);
-angComponentGenerator.angGenerateComponent(ang_scaffold);
-angRouteGenerator.angGenerateRoute(ang_scaffold);
-
-
-for(var aux in inputul){
-
-  var input, modelName, pluralName;
-  var obj_prop;
-  var types = [];
-
-  input = inputul[aux];
-
-  for (var key in input) {
-    if (input.hasOwnProperty(key)) {
-        modelName = key;
-        pluralName = pluralize.plural(modelName);
-        obj_prop = input[key];
-    }
-  }
-
-
-  for (var key in obj_prop) {
-    if (allowedTypes.indexOf(obj_prop[key].toLowerCase()) === -1) {
-      showUsage("Property type" + obj_prop[key] + "not allowed");
-      process.exit(1);
-    }
-    if (obj_prop.hasOwnProperty(key)) {
-          types.push({
-            name: key,
-            type: obj_prop[key]
-          });
-    }
-}
   if (!fs.existsSync('models'))
     fs.mkdir('models');
   if (!fs.existsSync('controllers'))
     fs.mkdir('controllers');
 
-  modelGenerator.generateModel(modelName, pluralName, types, function(err) {
-    if (err) {
-      showUsage('There was a problem generating the model file.');
-    }
-    console.log('Model file generated.');
-});
+  for (var aux in client_input) {
 
-   controllerGenerator.generateController(modelName, pluralName, types, function(err) {
-    if (err) {
-      showUsage('There was a problem generating the controller file.');
-    }
-    console.log('Generated controller');
-  });
+    var ang_scaffold = client_input[aux];
 
-}
+    angModelGenerator.angGenerateModel(ang_scaffold);
+    angDetailGenerator.angGenerateDetail(ang_scaffold);
+    angServiceGenerator.angGenerateService(ang_scaffold);
+    angComponentGenerator.angGenerateComponent(ang_scaffold);
+  }
+
+  angRouteGenerator.angGenerateRoute(ang_scaffold);
+  angAppGenerator.angGenerateApp(ang_scaffold);
+
+
+  for (var aux in inputul) {
+
+    var input, modelName, pluralName;
+    var obj_prop;
+    var types = [];
+
+    input = inputul[aux];
+
+    for (var key in input) {
+      if (input.hasOwnProperty(key)) {
+          modelName = key;
+          pluralName = pluralize.plural(modelName);
+          obj_prop = input[key];
+      }
+    }
+
+
+    for (var key in obj_prop) {
+      if (allowedTypes_server.indexOf(obj_prop[key].toLowerCase()) === -1) {
+        showUsage("Property type" + obj_prop[key] + "not allowed");
+        process.exit(1);
+      }
+      if (obj_prop.hasOwnProperty(key)) {
+            types.push({
+              name: key,
+              type: obj_prop[key]
+            });
+      }
+    }
+
+
+    modelGenerator.generateModel(modelName, pluralName, types, function(err) {
+      if (err) {
+        showUsage('There was a problem generating the model file.');
+      }
+      console.log('Model file generated.');
+    });
+
+     controllerGenerator.generateController(modelName, pluralName, types, function(err) {
+      if (err) {
+        showUsage('There was a problem generating the controller file.');
+      }
+      console.log('Generated controller');
+    });
+  }
 
 function showUsage(err) {
 
